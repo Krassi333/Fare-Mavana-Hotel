5cce0b59a3bfebff67d5fc9e259e2233ec2caab8
Krasimira Valcheva <krasimira.valcheva88@gmail.com>
2023-11-22 16:17:09 +0200

    Add header com ponent

---
+++ Fare-Mavana-Hotel/src/components/Header/Header.jsx
@@ -0,0 +1,23 @@
+
+export default function Header() {
+    return (
+        <>
+            <div className="nav-container">
+                <h1>
+                    <span>Fare Mavana Hotel</span> </h1>
+                <nav>
+                    <ul>
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/roomsCatalog">Rooms and Suites</a></li>
+                        <li><a href="/restaurant">Ambrozia Reataurant</a></li>
+                        <li><a href="/addRoom">Add Room</a></li>
+                        <li><a href="/login">Login</a></li>
+                        <li><a href="/register">Register</a></li>
+                    </ul>
+                </nav>
+
+            </div>
+
+        </>
+    )
+}
