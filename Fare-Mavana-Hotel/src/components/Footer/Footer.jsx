787e6fc4176895e9f0d2f1d679c1ace616e5d4d8
Krasimira Valcheva <krasimira.valcheva88@gmail.com>
2023-11-22 16:17:22 +0200

    Add footer component

---
+++ Fare-Mavana-Hotel/src/components/Footer/Footer.jsx
@@ -0,0 +1,86 @@
+export default function Footer() {
+    return (
+        <>
+           
+            <meta charSet="utf-8" />
+            <meta
+                name="viewport"
+                content="width=device-width, initial-scale=1, shrink-to-fit=no"
+            />
+            <link
+                href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900"
+                rel="stylesheet"
+            />
+            <link rel="stylesheet" href="css/ionicons.min.css" />
+            <link rel="stylesheet" href="css/style.css" />
+         
+            <footer className="footer-07">
+                <div className="container">
+                    <div className="row justify-content-center">
+                        <div className="col-md-12 text-center">
+                            <h2 className="footer-heading">
+                                <a href="/" className="logo">
+                                    FareManavaHotel.com
+                                </a>
+                            </h2>
+                            <p className="menu">
+                                <a href="/">Home</a>
+                                <a href="#">Agent</a>
+                                <a href="#">About</a>
+                                <a href="#">Listing</a>
+                                <a href="#">Blog</a>
+                                <a href="#">Contact</a>
+                            </p>
+                            <ul className="ftco-footer-social p-0">
+                                <li className="ftco-animate">
+                                    <a
+                                        href="#"
+                                        data-toggle="tooltip"
+                                        data-placement="top"
+                                        title="Twitter"
+                                    >
+                                        <span className="ion-logo-twitter" />
+                                    </a>
+                                </li>
+                                <li className="ftco-animate">
+                                    <a
+                                        href="#"
+                                        data-toggle="tooltip"
+                                        data-placement="top"
+                                        title="Facebook"
+                                    >
+                                        <span className="ion-logo-facebook" />
+                                    </a>
+                                </li>
+                                <li className="ftco-animate">
+                                    <a
+                                        href="#"
+                                        data-toggle="tooltip"
+                                        data-placement="top"
+                                        title="Instagram"
+                                    >
+                                        <span className="ion-logo-instagram" />
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="row mt-5">
+                        <div className="col-md-12 text-center">
+                            <p className="copyright">
+                                {/* Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. */}
+                                Copyright © All rights reserved | This template is made with{" "}
+                                <i className="ion-ios-heart" aria-hidden="true" /> by{" "}
+                                <a href="/" target="_blank">
+                                    FareManavaHotel.com
+                                </a>
+                                {/* Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. */}
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </>
+
+    )
+}
