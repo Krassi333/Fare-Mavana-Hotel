ac348fd28454bb2b3bc24212163655dca460a6ba
Krasimira Valcheva <krasimira.valcheva88@gmail.com>
2023-11-22 16:16:52 +0200

    Add Home component

---
+++ Fare-Mavana-Hotel/src/components/Home/Home.jsx
@@ -0,0 +1,27 @@
+export default function Home() {
+    return (
+        <>
+         
+
+            <div className="banner ">
+                <h3 ><p>'Ia ora na, Maeva!</p></h3>
+                <h2 className="the_title">Welcome into the heart of the mythic island</h2>
+
+                <div >
+                    <p className='text-part'>Our Polynesian hotel has a large tropical garden on
+                        the moutain side where all the rooms are nestled.<br />
+                        On the sea side, the restaurant, the beach bar and the bar dancing surround
+                        our swimming pool and offer a direct access to the beautiful white</p>
+                    <p className='text-part'>  sand beach and an amazing view of motu Piti Uu’Uta.
+                        Twice a week, the bar and restaurant welcome local bands. This traditional
+                        Polynesian atmosphere will immerge you in the Tahitian way of entertaining.
+                        <br />
+                        <strong>Discover the real Tahitian way of life at the Royal Bora Bora.
+                        </strong></p>
+                </div>
+
+            </div>
+
+        </>
+    )
+}
