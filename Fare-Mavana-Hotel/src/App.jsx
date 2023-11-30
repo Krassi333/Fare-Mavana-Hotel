b76a1c9e27c1dfd5aff6f8b19ecf8e3d7abc2cef
Krasimira Valcheva <krasimira.valcheva88@gmail.com>
2023-11-30 12:44:11 +0200

    Add  auth context to App.jsx

--- Fare-Mavana-Hotel/src/App.jsx
+++ Fare-Mavana-Hotel/src/App.jsx
@@ -8,21 +8,32 @@ import TypeChoise from './components/Catalog/TypeChoise/TypeChoise'
 import AdminPanel from './components/Login/AdminPanel/AdminPanel'
 import AddRoom from './components/Add room/AddRoom'
 import RoomDetails from './components/RoomDetails/RoomDetails'
+import Login from './components/Login/Login'
+import { useState } from 'react'
+import AuthContext from './context/authContext'
 
 function App() {
+  const [auth, setAuth] = useState({});
 
+  const loginSubmitHandler = (values) => {
+    console.log(values);
+  }
 
   return (
     <>
-      <Header sticky="top" />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/addRoom" element={<AddRoom />} />
-        <Route path="/roomsCatalog" element={<TypeChoise />} />
-        <Route path="/rooms/:id" element={<RoomDetails />} />
-      </Routes>
-      <Footer />
 
+      <AuthContext.Provider value={loginSubmitHandler}>
+
+        <Header sticky="top" />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/addRoom" element={<AddRoom />} />
+          <Route path="/roomsCatalog" element={<TypeChoise />} />
+          <Route path="/rooms/:id" element={<RoomDetails />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </AuthContext.Provider>
     </>
 
   )
