3d79d6037cd3b50bc04a5266af942211d51ba10e
Krasimira Valcheva <krasimira.valcheva88@gmail.com>
2023-11-30 12:40:00 +0200

    Update navigation using Link

--- Fare-Mavana-Hotel/src/components/Header/Header.jsx
+++ Fare-Mavana-Hotel/src/components/Header/Header.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom"
 
 export default function Header() {
     return (
@@ -7,12 +8,14 @@ export default function Header() {
                     <span>Fare Mavana Hotel</span> </h1>
                 <nav>
                     <ul>
-                        <li><a href="/">Home</a></li>
-                        <li><a href="/roomsCatalog">Rooms and Suites</a></li>
-                        <li><a href="/restaurant">Ambrozia Reataurant</a></li>
-                        <li><a href="/addRoom">Add Room</a></li>
-                        <li><a href="/login">Login</a></li>
-                        <li><a href="/register">Register</a></li>
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/roomsCatalog">Rooms and Suites</Link></li>
+                        <li><Link to="/restaurant">Ambrozia Reataurant</Link></li>
+                        <li><Link to="/addRoom">Add Room</Link></li>
+                        <li><Link to="/login">Login</Link></li>
+                        <li><Link to="/register">Register</Link></li>
+                        <li><Link to="/logout">Logout</Link></li>
+
                     </ul>
                 </nav>
 
