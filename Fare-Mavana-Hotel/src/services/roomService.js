89d1949c63deb51396a181148437637e36589d01
Krasimira Valcheva <krasimira.valcheva88@gmail.com>
2023-11-30 12:41:38 +0200

    Add get all rooms service

--- Fare-Mavana-Hotel/src/services/roomService.js
+++ Fare-Mavana-Hotel/src/services/roomService.js
@@ -13,3 +13,18 @@ export const create = async (roomData) => {
 
     return result;
 }
+
+export const getAll = async () => {
+    const responce = await fetch(`${baseUrl}/rooms`, { method: 'GET' });
+    const result = await responce.json();
+
+    return Object.values(result);
+}
+
+export const getOne = async (gameId) => {
+    const responce =await fetch(`${baseUrl}/rooms/${gameId}`, { method: 'GET' });
+console.log(responce);
+    const result = await responce.json();
+
+    return result;
+}
