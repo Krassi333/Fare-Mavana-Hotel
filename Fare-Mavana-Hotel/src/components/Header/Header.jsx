394ab5a56e079e41643a72c902f792e6f363a70d
Krasimira Valcheva <krasimira.valcheva88@gmail.com>
2023-12-01 11:57:21 +0200

    Separate navigation view according to  if there is logged user

--- Fare-Mavana-Hotel/src/components/Header/Header.jsx
+++ Fare-Mavana-Hotel/src/components/Header/Header.jsx
@@ -1,6 +1,10 @@
+import { useContext } from "react"
 import { Link } from "react-router-dom"
+import AuthContext from "../../context/authContext";
 
 export default function Header() {
+    const { isAuthenticated, username } = useContext(AuthContext);
+
     return (
         <>
             <div className="nav-container">
@@ -11,10 +15,21 @@ export default function Header() {
                         <li><Link to="/">Home</Link></li>
                         <li><Link to="/roomsCatalog">Rooms and Suites</Link></li>
                         <li><Link to="/restaurant">Ambrozia Reataurant</Link></li>
-                        <li><Link to="/addRoom">Add Room</Link></li>
-                        <li><Link to="/login">Login</Link></li>
-                        <li><Link to="/register">Register</Link></li>
-                        <li><Link to="/logout">Logout</Link></li>
+                        {isAuthenticated && (
+                            <>
+                                <li><Link to="/addRoom">Add Room</Link></li>
+                                <li><Link to="/logout">Logout</Link></li>
+                            </>
+                        )}
+
+                        {!isAuthenticated && (
+                            <>
+                                <li><Link to="/login">Login</Link></li>
+                                <li><Link to="/register">Register</Link></li>
+                            </>
+                        )}
+
+
 
                     </ul>
                 </nav>
