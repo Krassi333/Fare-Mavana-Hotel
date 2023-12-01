cf04a1dfb05e028cebcf5bda29845e4225fea965
Krasimira Valcheva <krasimira.valcheva88@gmail.com>
2023-12-01 16:39:59 +0200

    Create useLocalStorage hook

---
+++ Fare-Mavana-Hotel/src/hooks/useLocalStorage.js
@@ -0,0 +1,32 @@
+import { useState } from "react"
+
+export default function useLocalStorage(key, defaultValue) {
+    const [state, setState] = useState((defaultValue => {
+        const persistedState = localStorage.getItem(key);
+
+        if (persistedState) {
+            return JSON.parse(persistedState);
+        } else {
+            return defaultValue;
+        }
+    }));
+
+    const setLocalStorageState = (value) => {
+        setState(value);
+        let serializedValue;
+
+        if (typeof value === 'function') {
+            serializedValue = JSON.stringify(value(state));
+        } else {
+            serializedValue = JSON.stringify(value);
+
+        }
+
+        localStorage.setItem(key, serializestatedValue);
+    }
+
+    return [
+        state,
+        setLocalStorageState
+    ]
+}
