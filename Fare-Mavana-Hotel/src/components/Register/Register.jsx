f0a72bc629a8f19ef20baea3483ac72258429c2f
Krasimira Valcheva <krasimira.valcheva88@gmail.com>
2023-12-08 09:25:18 +0200

    Add validations to the register form

--- Fare-Mavana-Hotel/src/components/Register/Register.jsx
+++ Fare-Mavana-Hotel/src/components/Register/Register.jsx
@@ -1,30 +1,68 @@
 import { useContext } from "react";
 import useForm from "../../hooks/useForm";
 import AuthContext from "../../context/authContext";
+import { useFormik } from 'formik';
+
 
 export default function Register() {
 
-    const {registerSubmitHandler}=useContext(AuthContext);
+    const { registerSubmitHandler } = useContext(AuthContext);
 
-const RegisterFormKeys={
-    Email:'email',
-    Password:'password',
-    RePassword:'rePassword',
-    Username:'username'
-}
-    const { formValues, onChange, onSubmit } = useForm(registerSubmitHandler ,{
-        [RegisterFormKeys.Email]: '',
-        [RegisterFormKeys.Username]: '',
-        [RegisterFormKeys.Password]: '',
-        [RegisterFormKeys.RePassword]:'',
+    const RegisterFormKeys = {
+        Email: 'email',
+        Password: 'password',
+        RePassword: 'rePassword',
+        Username: 'username'
+    }
 
-    });
+
+    const validate = (values) => {
+        let errors = {};
+
+        if (!values.email) {
+            errors.email = 'Email is required!';
+        } else if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/i.test(values.email)) {
+            errors.email = 'Invalid email format!'
+        }
+
+        if (!values.username) {
+            errors.username = 'Username is required!';
+        }
+
+        if (!values.password) {
+            errors.password = 'Password is required!';
+        }
+
+        if (!values.rePassword) {
+            errors.rePassword = 'This field is required!';
+        } else if (values.rePassword != values.password) {
+            errors.rePassword = "Password and repeated password don't match!";
+
+        }
+        return errors;
+    }
+
+
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            username: '',
+            password: '',
+            rePassword: ''
+        },
+
+        onSubmit: (values) => {
+            registerSubmitHandler(values);
+        },
+
+        validate
+    })
 
 
     return (
 
         <section id="registerPage">
-            <form className="registerForm" onSubmit={onSubmit} >
+            <form className="registerForm" onSubmit={formik.handleSubmit} >
 
                 <div>
                     <h2>Register</h2>
@@ -32,11 +70,13 @@ const RegisterFormKeys={
                         <label htmlFor="email">Email:</label>
                         <input
                             name={RegisterFormKeys.Email}
-                            id="username"
+                            id="email"
                             type="text"
-                            onChange={onChange}
-                            value={formValues[RegisterFormKeys.Email]}
+                            onBlur={formik.handleBlur}
+                            onChange={formik.handleChange}
+                            value={formik.values.name}
                         />
+                         { formik.touched.email && formik.errors.email &&(<p className='error'>{formik.errors.email}</p>) }
                     </div>
 
                     <div className="username">
@@ -45,9 +85,12 @@ const RegisterFormKeys={
                             name={RegisterFormKeys.Username}
                             id="username"
                             type="text"
-                            onChange={onChange}
-                            value={formValues[RegisterFormKeys.Username]}
+                            onBlur={formik.handleBlur}
+                            onChange={formik.handleChange}
+                            value={formik.values.name}
                         />
+                         { formik.touched.username && formik.errors.username &&(<p className='error'>{formik.errors.username}</p>) }
+
                     </div>
 
 
@@ -57,10 +100,13 @@ const RegisterFormKeys={
                             name={RegisterFormKeys.Password}
                             id="password"
                             type="text"
-                            onChange={onChange}
-                            value={formValues[RegisterFormKeys.Password]}
+                            onBlur={formik.handleBlur}
+                            onChange={formik.handleChange}
+                            value={formik.values.name}
 
                         />
+                         { formik.touched.password && formik.errors.password &&(<p className='error'>{formik.errors.password}</p>) }
+
                     </div>
 
                     <div className="rePassword">
@@ -69,10 +115,13 @@ const RegisterFormKeys={
                             name={RegisterFormKeys.RePassword}
                             id="rePassword"
                             type="text"
-                            onChange={onChange}
-                            value={formValues[RegisterFormKeys.RePassword]}
+                            onBlur={formik.handleBlur}
+                            onChange={formik.handleChange}
+                            value={formik.values.name}
 
                         />
+                         { formik.touched.rePassword && formik.errors.rePassword &&(<p className='error'>{formik.errors.rePassword}</p>) }
+
                     </div>
 
 
