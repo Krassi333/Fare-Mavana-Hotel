9eb3021a56b18963fdfcd1fe6245eba93f75e201
Krasimira Valcheva <krasimira.valcheva88@gmail.com>
2023-12-08 09:25:00 +0200

    Add validations to the login form

--- Fare-Mavana-Hotel/src/components/Login/Login.jsx
+++ Fare-Mavana-Hotel/src/components/Login/Login.jsx
@@ -1,26 +1,54 @@
 import { useContext } from "react";
-import useForm from "../../hooks/useForm";
-import authContext from "../../context/authContext";
 import AuthContext from "../../context/authContext";
+import { useFormik } from 'formik';
 
 export default function Login() {
 
-    const {loginSubmitHandler}=useContext(AuthContext);
+    const { loginSubmitHandler } = useContext(AuthContext);
+//console.log('here');
+    const LoginFormKeys = {
+        Email: 'email',
+        Password: 'password'
+    }
 
-const LoginFormKeys={
-    Email:'email',
-    Password:'password'
-}
-    const { formValues, onChange, onSubmit } = useForm(loginSubmitHandler ,{
-        [LoginFormKeys.Email]: '',
-        [LoginFormKeys.Password]: ''
-    });
+    const validate = (values) => {
+        let errors = {};
 
+        if (!values.email) {
+            errors.email = 'Email is required!';
+        } else if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/i.test(values.email)) {
+            errors.email = 'Invalid email format!'
+        }
+
+        if (!values.password) {
+            errors.password = 'Password is required!';
+        }
+
+        console.log(errors);
+        return errors;
+    }
+    // const { formValues, onChange, onSubmit } = useForm(loginSubmitHandler ,{
+    //     [LoginFormKeys.Email]: '',
+    //     [LoginFormKeys.Password]: ''
+    // });
+
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            password: ''
+        },
+
+        onSubmit: (values) => {
+            loginSubmitHandler(values);
+        },
+
+        validate
+    })
 
     return (
 
         <section id="loginPage">
-            <form className="loginForm" onSubmit={onSubmit} >
+            <form className="loginForm" onSubmit={formik.handleSubmit} >
 
                 <div>
                     <h2>Login</h2>
@@ -30,9 +58,11 @@ const LoginFormKeys={
                             name={LoginFormKeys.Email}
                             id="username"
                             type="text"
-                            onChange={onChange}
-                            value={formValues[LoginFormKeys.Email]}
+                            onBlur={formik.handleBlur}
+                            onChange={formik.handleChange}
+                            value={formik.values.name}
                         />
+                        { formik.touched.email && formik.errors.email &&(<p className='error'>{formik.errors.email}</p>) }
                     </div>
 
 
@@ -42,10 +72,12 @@ const LoginFormKeys={
                             name={LoginFormKeys.Password}
                             id="password"
                             type="text"
-                            onChange={onChange}
-                            value={formValues[LoginFormKeys.Password]}
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                            value={formik.values.name}
 
                         />
+                        {formik.touched.password && formik.errors.password && (<p className='error' >{formik.errors.password}</p>) }
                     </div>
 
 
