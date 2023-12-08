315d771b33dfcc5f1c7d02ef9f861b4c9b81e3cb
Krasimira Valcheva <krasimira.valcheva88@gmail.com>
2023-12-08 09:23:28 +0200

    Add authGuard to the routes

--- Fare-Mavana-Hotel/src/App.jsx
+++ Fare-Mavana-Hotel/src/App.jsx
@@ -6,15 +6,18 @@ import { AuthProvider } from './context/authContext'
 import Footer from './components/Footer/Footer'
 import Header from './components/Header/Header'
 import Home from './components/Home/Home'
-import TypeChoise from './components/Catalog/TypeChoise/TypeChoise'
-import AdminPanel from './components/Login/AdminPanel/AdminPanel'
 import AddRoom from './components/Add room/AddRoom'
 import RoomDetails from './components/RoomDetails/RoomDetails'
-import Login from './components/Login/Login'
+//import Login from './components/Login/Login'
+
+
 import Register from './components/Register/Register'
 import Logout from './components/Logout/logout'
 import EditRoom from './components/EditRoom/EditRoom'
 import ErrorBoundary from './components/ErrorBoudary/ErrorBoundary'
+import AuthGuard from './components/Guards/AuthGuard'
+import Login from './components/Login/Login'
+import Catalog from './components/Catalog/Catalog'
 
 function App() {
 
@@ -27,15 +30,18 @@ function App() {
           <Header sticky="top" />
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/addRoom" element={<AddRoom />} />
-            <Route path="/roomsCatalog" element={<TypeChoise />} />
-            <Route path="/rooms/:id" element={<RoomDetails />} />
+            <Route path="/roomsCatalog" element={<Catalog />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
-            <Route path="/logout" element={<Logout />} />
-            <Route path="/rooms/:id/edit" element={<EditRoom />} />
+
+            <Route element={<AuthGuard />}>
+              <Route path="/addRoom" element={<AddRoom />} />
+              <Route path="/rooms/:id" element={<RoomDetails />} />
+              <Route path="/logout" element={<Logout />} />
+              <Route path="/rooms/:id/edit" element={<EditRoom />} />
+            </Route>
           </Routes>
-          
+
           <Footer />
         </AuthProvider>
       </ErrorBoundary>
