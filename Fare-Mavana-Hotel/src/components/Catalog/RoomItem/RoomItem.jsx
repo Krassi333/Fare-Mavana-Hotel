0dac176df8bd34651150caec9447d13ccb63e7f3
Krasimira Valcheva <krasimira.valcheva88@gmail.com>
2023-12-08 09:26:19 +0200

    Create roomItem

--- Fare-Mavana-Hotel/src/components/Catalog/RoomItem/RoomItem.jsx
+++ Fare-Mavana-Hotel/src/components/Catalog/RoomItem/RoomItem.jsx
@@ -1,26 +1,112 @@
 import React from "react";
-import styles from "./RoomItem.module.css";
-import { Link } from "react-router-dom";
+import { styled } from '@mui/material/styles';
+import ButtonBase from '@mui/material/ButtonBase';
+import Typography from '@mui/material/Typography';
+import { useNavigate } from 'react-router-dom';
+import * as styles from './RoomItem.module.css';
+
+const ImageButton = styled(ButtonBase)(({ theme }) => ({
+    position: 'relative',
+    height: 500,
+    [theme.breakpoints.down('sm')]: {
+        width: '100% !important', // Overrides inline-style
+        height: 100,
+    },
+    '&:hover, &.Mui-focusVisible': {
+        zIndex: 1,
+        '& .MuiImageBackdrop-root': {
+            opacity: 0.15,
+        },
+        '& .MuiImageMarked-root': {
+            opacity: 0,
+        },
+        '& .MuiTypography-root': {
+            border: '4px solid currentColor',
+        },
+    },
+}));
+
+const ImageSrc = styled('span')({
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center 40%',
+});
+
+const Image = styled('span')(({ theme }) => ({
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: theme.palette.common.white,
+}));
+
+const ImageBackdrop = styled('span')(({ theme }) => ({
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    backgroundColor: theme.palette.common.black,
+    opacity: 0.4,
+    transition: theme.transitions.create('opacity'),
+}));
+
+const ImageMarked = styled('span')(({ theme }) => ({
+    height: 3,
+    width: 18,
+    backgroundColor: theme.palette.common.white,
+    position: 'absolute',
+    bottom: -2,
+    left: 'calc(50% - 9px)',
+    transition: theme.transitions.create('opacity'),
+}));
 
 export default function RoomItem({
-    title,
-    imageUrl,
-    description,
-    _id
+    room
 }) {
-    return (
-        <div className={styles.card}>
-            <div className={styles.imgContainer}>
-                <img src={imageUrl} alt="Card image cap" />
-                <a href="#">
-                    <div className="mask rgba-white-slight" />
-                </a>
-            </div>
+    const navigate = useNavigate();
 
-            <div className="card-body">
-                <h4 className="card-title">{title}</h4>
-                <Link to={`/rooms/${_id}`} className={styles.btn}>Read more</Link>
-            </div>
-        </div>
+    const onRoomChoiseHandler = () => {
+        navigate(`/rooms/${room._id}`)
+    }
+
+    return (
+        <ImageButton
+            onClick={onRoomChoiseHandler}
+            focusRipple
+            key={room.title}
+            style={{
+                width: '40%',
+                margin: '1em 3rem',
+                border: '2px solid black'
+            }}
+        >
+            <ImageSrc className={styles.background} style={{ backgroundImage: `url(${room.imageUrl})` }} />
+            <ImageBackdrop className="MuiImageBackdrop-root" />
+            <Image>
+                <Typography
+                    component="span"
+                    variant="subtitle1"
+                    color="inherit"
+                    sx={{
+                        position: 'relative',
+                        p: 4,
+                        pt: 2,
+                        pb: (theme) => `calc(${theme.spacing(1)} + 6px)`,
+                    }}
+                >
+                    {room.title}
+                    <ImageMarked className="MuiImageMarked-root" />
+                </Typography>
+            </Image>
+        </ImageButton>
     )
 }
